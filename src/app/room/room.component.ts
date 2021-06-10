94798f192a4ac6517a9fa36ebb6edc8945698f8a
kevnguy <56405177+kevnguy@users.noreply.github.com>
2021-06-09 23:17:04 -0700

    Delete random field

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ɵCompiler_compileModuleSync__POST_R3__ } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
